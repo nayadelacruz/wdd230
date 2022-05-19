2cf1491ae2999046f84f3ad5de06e58b59b82011
nayadelacruz <nayadelacruz@byui.edu>
2022-05-19 14:42:12 -0600

    update

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -26,3 +26,19 @@ const lastUpdated=`Last Updated: ${update}`;
 
 document.getElementById("update").innerHTML = lastUpdated;
 
+/* banner */
+let d = new Date().getDay;
+
+const banner = document.getElementById("banner");
+if (d === 1 || d === 2) {
+  banner.style.display = "block";
+}
+
+const close = document.querySelector('close');
+
+close.addEventListener("click", () => {
+  banner.style.display = "none";
+})
+
+
+
